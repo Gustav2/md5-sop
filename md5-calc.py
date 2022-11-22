efa2443e209da805b915dee1ed889fe702d3223e
Gustav2 <gustavnybro@protonmail.com>
2022-11-23 00:19:56 +0100

    I give up.. it dosen't work

--- md5-calc.py
+++ md5-calc.py
@@ -50,16 +50,22 @@ D – 76543210
 from math import sin
 
 
+def bitwise_not(n):
+    temp = "{0:b}".format(n).replace("0", "x").replace("1", "0").replace("x", "1")
+    return int(temp, 2)
+
+
 class MD5:
 
-    def __init__(self, message="Gustavs MD5 SOP"):
+    def __init__(self, message="MD5"):
         self.message = message
-        self.a = int("01234567", 16)
-        self.b = int("89abcdef", 16)
-        self.c = int("fedcba98", 16)
-        self.d = int("76543210", 16)
+        self.a = "01234567"
+        self.b = "89abcdef"
+        self.c = "fedcba98"
+        self.d = "76543210"
         self.bin_message = self.message_padding()
-        self.message_array = self.message_array()
+        self.message_array = self.create_message_array()
+        self.shift_amount = [7, 12, 17, 22] * 4 + [5, 9, 14, 20] * 4 + [4, 11, 16, 23] * 4 + [6, 10, 15, 21] * 4
 
     def message_padding(self):
         bin_message = ''.join(format(ord(i), 'b').zfill(8) for i in self.message)
@@ -69,48 +75,106 @@ class MD5:
 
         assert len(bin_message) == 512
 
-        return bin_message
+        return str(bin_message)
 
-    def message_array(self):
-        bin_msg = [self.bin_message[i:i+32] for i in range(0, 512, 32)]
-        return [hex(int(i, 2))[2:].zfill(8) for i in bin_msg]
-
+    def create_message_array(self):
+        bin_msg = [self.bin_message[i:i + 32] for i in range(0, 512, 32)]
+        return [str(hex(int(i, 2))[2:]).zfill(8) for i in bin_msg]
 
     def f(self):
-        return hex((self.b & self.c) | (~self.b & self.d))[2:]
 
-    def moda(self, x, y, z="100000000"):
+        return hex((int(self.b, 16) & int(self.c, 16)) | (bitwise_not(int(self.b, 16)) & int(self.d, 16)))[2:].zfill(8)
+
+    def g(self):
+        return hex((int(self.b, 16) & int(self.d, 16)) | (int(self.c, 16) & bitwise_not(int(self.d, 16))))[2:].zfill(8)
+
+    def h(self):
+        return hex(int(self.b, 16) ^ int(self.c, 16) ^ int(self.d, 16))[2:].zfill(8)
+
+    def i(self):
+
+        return hex(int(self.c, 16) ^ (int(self.b, 16) | bitwise_not(int(self.d, 16))))[2:].zfill(8)  # BD45C274
+
+    def mod_add(self, x, y):
         x = int(x, 16)
         y = int(y, 16)
-        z = int(z, 16)
-        return hex((x + y) % z)[2:]
+        z = pow(2, 32)
+
+        return hex((x + y) % z)[2:].zfill(8)
 
     def constant(self, i):
-        return hex(int(abs(sin(i + 1)) * 2 ** 32))[2:]
+        return hex(int(abs(sin(i + 1)) * 2 ** 32))[2:].zfill(8)
 
-    def bitshift(self, n):
+    def bitshift(self, n, i):
         temp = int(n, 16)
-        return hex(temp << 7 | temp >> 25)[4:]
+
+        temp = ((temp << self.shift_amount[i]) & 0xFFFFFFFF) | (temp >> (32 - self.shift_amount[i]))
+        return hex(temp)[2:].zfill(8)
+
+    def round1(self):
+        for i in range(16):
+            self.a = self.mod_add(self.a, self.f())
+            self.a = self.mod_add(self.a, self.message_array[i])
+            self.a = self.mod_add(self.a, self.constant(i))
+            self.a = self.bitshift(self.a, i)
+            self.a = self.mod_add(self.a, self.b)
+
+            self.a, self.b, self.c, self.d = self.d, self.a, self.b, self.c
+
+    def round2(self):
+        for i in range(16, 32):
+            self.a = self.mod_add(self.a, self.g())
+            self.a = self.mod_add(self.a, self.message_array[(1 + 5 * i) % 16])
+            self.a = self.mod_add(self.a, self.constant(i))
+            self.a = self.bitshift(self.a, i)
+            self.a = self.mod_add(self.a, self.b)
+            self.a, self.b, self.c, self.d = self.d, self.a, self.b, self.c
+
+    def round3(self):
+        for i in range(32, 48):
+            self.a = self.mod_add(self.a, self.h())
+            self.a = self.mod_add(self.a, self.message_array[(5 + 3 * i) % 16])
+            self.a = self.mod_add(self.a, self.constant(i))
+            self.a = self.bitshift(self.a, i)
+            self.a = self.mod_add(self.a, self.b)
+
+            self.a, self.b, self.c, self.d = self.d, self.a, self.b, self.c
+
+    def round4(self):
+        for i in range(48, 64):
+            self.a = self.mod_add(self.a, self.i())
+            self.a = self.mod_add(self.a, self.message_array[(7 * i) % 16])
+            self.a = self.mod_add(self.a, self.constant(i))
+            self.a = self.bitshift(self.a, i)
+            self.a = self.mod_add(self.a, self.b)
+            self.a, self.b, self.c, self.d = self.d, self.a, self.b, self.c
+
+    def md5(self):
+        self.round1()
+        self.round2()
+        self.round3()
+        self.round4()
+
+        print("gamererre " + self.a + " " + self.b + " " + self.c + " " + self.d)
+
+        oiva = "01234567"
+        oivb = "89abcdef"
+        oivc = "fedcba98"
+        oivd = "76543210"
+
+        self.a = self.mod_add(self.a, oiva)
+        self.b = self.mod_add(self.b, oivb)
+        self.c = self.mod_add(self.c, oivc)
+        self.d = self.mod_add(self.d, oivd)
+
+        return self.a + " " + self.b + " " + self.c + " " + self.d
 
 
+def main():
+    calc_hash = MD5("MD5")
+
+    print(calc_hash.md5())
 
 
-
-
-"""
-print(MD5().f())
-
-print(MD5().moda("01234567", "fedcba98"))
-print(MD5().moda("54686579", "ffffffff"))
-
-print(MD5().moda(MD5().constant(0), "54686578"))
-
-print(MD5().bitshift("2bd309f0"))
-
-print(MD5().moda("e984f815", "89abcdef"))
-
-
-print(MD5().message_array)
-
-"""
-
+if __name__ == "__main__":
+    main()
